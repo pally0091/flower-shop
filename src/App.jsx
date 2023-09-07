40793b735de1a93012e405b28708d0d5db926019
Debashis Roy <goodquote39@hotmail.com>
2023-09-08 00:45:00 +0600

    initialize project and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Banner from "./Components/Banner";
 
 function App() {
   return (
     <>
-      <h1>Botanical Bliss</h1>
+      <Banner></Banner>
     </>
   );
 }
---
+++ src/Components/Banner.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div>
+      <nav>
+        <h1>Botanical Bliss</h1>
+      </nav>
+    </div>
+  );
+};
+
+export default Banner;
